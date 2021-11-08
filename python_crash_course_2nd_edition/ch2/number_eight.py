b256d32cdad3befe4e62d590c930ae755ab5f53c
Peter Gallerani <peter.gallerani@parsons.com>
2021-11-07 21:31:19 -0700

    numbers

---
+++ python_crash_course_2nd_edition/ch2/number_eight.py
@@ -0,0 +1,7 @@
+print(4+4)
+
+print(10-2)
+
+print(4*2)
+
+print(int(64/8))
---
+++ python_crash_course_2nd_edition/ch2/numbers.py
@@ -0,0 +1,23 @@
+# addition
+print(1+1)
+
+# subtraction
+print(2-1)
+
+# multiplication
+print(2*2)
+
+# division
+print(4/2)
+
+# exponents or "to the power of"
+print(2**3)
+
+# order of operations
+print(4-3*9)
+# or
+print((4-3)*9)
+
+# floats, or decimal numbers
+print(.1+.9)
+print(.1+.99)
