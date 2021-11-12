c3f596a50a09c0ff199007f687cbbe06844bf66d
Peter Gallerani <peter.gallerani@parsons.com>
2021-11-11 18:38:42 -0700

    index errors

---
+++ python_crash_course_2nd_edition/ch3/index_error.py
@@ -0,0 +1,3 @@
+# trying to access an element not in the list
+motorcycles = ['harley', 'bmw', 'ktm']
+print(motorcycles[3])
---
+++ python_crash_course_2nd_edition/ch3/out_of_range_error.py
@@ -0,0 +1,3 @@
+# you can always use index -1 to return the last element, unless the list is empty
+list = []
+print(list[-1])
