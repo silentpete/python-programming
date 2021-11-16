000fe4b0d52155ec6b2cc8c9d1ff8158e1883a6d
Peter Gallerani <peter.gallerani@parsons.com>
2021-11-15 19:41:27 -0700

    for loop

---
+++ python_crash_course_2nd_edition/ch4/animals.py
@@ -0,0 +1,8 @@
+hairy_animals = ['lions', 'tigers', 'bears']
+for animal in hairy_animals:
+    print(animal)
+
+for animal in hairy_animals:
+    print(f"{animal.title()} are not good pets.")
+
+print("These wild animals are not domesticatable.")
---
+++ python_crash_course_2nd_edition/ch4/pizzas.py
@@ -0,0 +1,5 @@
+pizzas = ['pepperoni', 'chicken parm with tomato', 'ham and pineapple']
+
+for pizza in pizzas:
+    print(f"Man! I love a good {pizza}, pizza")
+print("I really love pizza!")
